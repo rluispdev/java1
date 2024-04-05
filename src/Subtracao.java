040076a252f031c317e2ae49036fe7d2eab2df59
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-05 07:57:26 -0300

    Criar uma classe Subtração para imprimir o resultado de 10 - 5 no console.OOP

---
+++ src/Subtracao.java
@@ -0,0 +1,10 @@
+public class Subtracao {
+    int minuend = 10;
+    int subtrahend = 5;
+    int difference = minuend - subtrahend;
+
+    public static void main(String[] args){
+        Subtracao sub = new Subtracao();
+        System.out.println(sub.difference);
+    }
+}
