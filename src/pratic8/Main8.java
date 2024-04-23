8e28df1674ff080447bb521a2fbb4bf360427120
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-23 08:22:36 -0300

    Add three people in the ArrayList.

--- src/pratic8/Main8.java
+++ src/pratic8/Main8.java
@@ -14,14 +14,17 @@ public class Main8 {
         person2.setName("Rafael");
         person2.setAge(32);
 
+        var person3 = new Person();
+        person3.setName("Neymar");
+        person3.setAge(29);
+
         System.out.println(person);
 
         ArrayList<Person> namesPerson = new ArrayList<>();
         namesPerson.add(person);
         namesPerson.add(person2);
+        namesPerson.add(person3);
 
         System.out.println(namesPerson);
-
     }
-
 }
